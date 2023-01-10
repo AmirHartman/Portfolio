6a8c3e213e16d65b9f1a16f5ed6081c4b6f34fd0
AmirHartman <110243224+AmirHartman@users.noreply.github.com>
2023-01-10 08:01:00 +0200

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,38 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+# resources = {
+#     "water": 300,
+#     "milk": 200,
+#     "coffee": 100,
+#     "money": 50
+# }
+# for ingredient_checked in MENU["latte"]['ingredients']:
+#     # if MENU["latte"]['ingredients'][ingredient_checked] > resources[ingredient_checked]:
+#     print("Ingredient in the menu:")
+#     print(MENU["latte"]['ingredients'][ingredient_checked])
+#     print("Ingredient in the resource:")
+#     print(resources[ingredient_checked])
